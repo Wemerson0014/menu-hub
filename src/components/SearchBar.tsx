4ee39b1cf43a510e87e0842539e8023fb1e5f402
Wemerson Fernandes <wemersonf.rangel@outlook.com>
2026-08-25 10:34:34 -0300

    style: improve header and search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,8 +1,10 @@
+import "./SearchBar.css";
+
 function SearchBar() {
   return (
-    <form>
-      <input type="text" placeholder="Buscar..." />
-      <button>Pesquisar</button>
+    <form className="search-bar">
+      <input className="search-bar input" type="text" placeholder="Buscar..." />
+      <button className="search-bar button">Pesquisar</button>
     </form>
   );
 }
