461d0635c7d15b0e7e828163d3a7ad8aaf8460af
Wemerson Fernandes <wemersonf.rangel@outlook.com>
2026-08-26 10:01:52 -0300

    feat: add hero section and color palette

---
+++ src/components/Hero.tsx
@@ -0,0 +1,13 @@
+import "./Hero.css";
+
+function Hero() {
+  return (
+    <section className="hero">
+      <h1>Assados da Debora</h1>
+      <p>O sabor do final de semana!</p>
+      <button>Ver Cardápio</button>
+    </section>
+  );
+}
+
+export default Hero;
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -3,8 +3,8 @@ import "./SearchBar.css";
 function SearchBar() {
   return (
     <form className="search-bar">
-      <input className="search-bar input" type="text" placeholder="Buscar..." />
-      <button className="search-bar button">Pesquisar</button>
+      <input type="text" placeholder="Buscar..." />
+      <button>Pesquisar</button>
     </form>
   );
 }
