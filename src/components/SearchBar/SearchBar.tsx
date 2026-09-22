9494557079684cec3f813febc23ef59ae96ed0a4
Wemerson Fernandes <wemersonf.rangel@outlook.com>
2026-09-22 09:19:00 -0300

    feat: prepare search bar form handling

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,9 +1,22 @@
 import "./SearchBar.css";
+import { useState } from "react";
 
 function SearchBar() {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
-    <form className="search-bar">
-      <input type="text" placeholder="Buscar..." />
+    <form
+      id="form"
+      className="search-bar"
+      onSubmit={(event) => {
+        event.preventDefault();
+      }}
+    >
+      <input
+        type="text"
+        placeholder="Buscar..."
+        onChange={(event) => setSearchTerm(event.target.value)}
+      />
       <button>Pesquisar</button>
     </form>
   );
